0ce62c18a04b4e5b847fee81c0717486e92c5ed8
Sylvan <hooversy@oregonstate.edu>
2019-04-23 19:18:05 -0700

    tests for osm_download module

--- osm_multiplex/tests/test_osm_download.py
+++ osm_multiplex/tests/test_osm_download.py
@@ -1,3 +1,4 @@
+# third-party libraries
 import networkx as nx
 
 # local imports
@@ -42,7 +43,7 @@ class TestGenerateMultiplex:
 
 		test_multiplex = osm_download.generate_multiplex(test_area, test_modes)
 
-		assert isinstance(test_multiplex, networkx.classes.multidigraph.MultiDiGraph) == True)
+		assert isinstance(test_multiplex, networkx.classes.multidigraph.MultiDiGraph) == True
 
 	def test_bbox_multiplex(self):
 		"""
@@ -53,4 +54,20 @@ class TestGenerateMultiplex:
 
 		test_multiplex = osm_download.generate_multiplex(test_area, test_modes)
 
-		assert isinstance(test_multiplex, networkx.classes.multidigraph.MultiDiGraph) == True)
+		assert isinstance(test_multiplex, networkx.classes.multidigraph.MultiDiGraph) == True
+
+class TestMergeMultiplexNodes
+	"""
+	Tests if colocated nodes have edges connecting them
+	"""
+
+	def test_merge_multiplex_nodes(self):
+		separated_graph = nx.MultiDiGraph()
+		separated_graph.add_nodes_from(['A-1', 'A-2', 'B-1', 'B-2'])
+		separated_graph.add_edges_from([('A-1', 'A-2'), ('B-1', 'B-2')])
+
+		connected_graph = osm_download.merge_multiplex_nodes(separated_graph)
+
+		connected_edges = list(connected_graph.edges)
+		assert ('A-1', 'B-1', 0) in connected_edges
+		assert ('A-2', 'B-2', 0) in connected_edges
