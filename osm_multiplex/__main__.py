66ff950b99cec8c75426e20528fe556dd7be0e48
Sylvan <hooversy@oregonstate.edu>
2019-07-26 21:13:56 -0700

    added joined and testing
    joined uses pickled datasets that have already been joined
    testing modified the occupancy of dataset2 to induce anomaly; useful if using two identical datasets

--- osm_multiplex/__main__.py
+++ osm_multiplex/__main__.py
@@ -39,6 +39,18 @@ parser.add_argument('-ad', '--anomaly-detect',
                     help='Run mode counts anomaly detection'
                     )
 
+parser.add_argument('-j', '--joined',
+                    action='store_true',
+                    required=False,
+                    help='Use already joined datasets'
+                    )
+
+parser.add_argument('-t', '--testing',
+                    action='store_true',
+                    required=False,
+                    help='If using identical datasets, modifies 2nd set during processing'
+                    )
+
 parser.add_argument('-d1', '--dataset1',
                     type=str,
                     default=None,
@@ -213,22 +225,27 @@ if args.graph == True:
     multiplex = generate_multiplex(args.graph_area, [args.graph_modes])
     write_gpickle(multiplex, './osm_multiplex/data/multiplex.gpickle')
 elif args.anomaly_detect == True:
-    print("Merging datasets")
-    likely_pairs = process_data(args.dataset1, args.dataset2, run_npmi=args.npmi,
-    element_id1=args.element_id1, timestamp1=args.timestamp1, session_start1=args.session_start1, session_end1=args.session_end1,
-    boardings1=args.boardings1, alightings1=args.alightings1, occupancy1=args.occupancy1,
-    lat1=args.latitude1, lon1=args.longitude1,
-    element_id2=args.element_id2, timestamp2=args.timestamp2, session_start2=args.session_start2, session_end2=args.session_end2,
-    boardings2=args.boardings2, alightings2=args.alightings2, occupancy2=args.occupancy2,
-    lat2=args.latitude2, lon2=args.longitude2
-    )
+    if args.joined == True:
+        print("Loading joined dataset")
+        likely_pairs = pickle.load(open('./osm_multiplex/data/likely_pairs.pickle', 'rb'))
+    else:
+        print("Merging datasets")
+        likely_pairs = process_data(args.dataset1, args.dataset2, run_npmi=args.npmi,
+        element_id1=args.element_id1, timestamp1=args.timestamp1, session_start1=args.session_start1, session_end1=args.session_end1,
+        boardings1=args.boardings1, alightings1=args.alightings1, occupancy1=args.occupancy1,
+        lat1=args.latitude1, lon1=args.longitude1,
+        element_id2=args.element_id2, timestamp2=args.timestamp2, session_start2=args.session_start2, session_end2=args.session_end2,
+        boardings2=args.boardings2, alightings2=args.alightings2, occupancy2=args.occupancy2,
+        lat2=args.latitude2, lon2=args.longitude2)
+        pickle.dump(likely_pairs, open('./osm_multiplex/data/likely_pairs.pickle', 'wb'))
     print("Preprocessing for LSTM")
     preprocessed = preprocess(likely_pairs)
+    pickle.dump(preprocessed, open('./osm_multiplex/data/lstm_preprocessed.pickle', 'wb'))
     print("LSTM processing")
     if args.rolling == True:
         anomalies = anomaly_detect(preprocessed, "rolling", locations=args.locations)
     else:
-        anomalies = anomaly_detect(preprocessed, locations=args.locations)
+        anomalies = anomaly_detect(preprocessed, locations=args.locations, testing=args.testing)
     pickle.dump(anomalies, open('./osm_multiplex/data/anomaly_results.pickle', 'wb'))
 else:
     raise Exception('No action specified')
